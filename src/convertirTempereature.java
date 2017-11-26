5ed5bc06cb28287a649f5f5988bb1003705254a9
JeremieLoison <32833061+JeremieLoison@users.noreply.github.com>
2017-11-26 17:26:42 +0100

    Exercice pour travailler sur les variables, les conditions, les boucles

---
+++ src/convertirTempereature.java
@@ -0,0 +1,37 @@
+
+
+
+/**
+ * J'ai réaliser cette application en suivant le tuto d'OpenClassRoom
+ * afin de travailler les variables, les conditions et les boucles.
+ */
+
+/**
+ *
+ * @author Jérémie-Loison
+ * Je suis en formation de Développeur Logiciel
+ * dans l'établissement m2i-Formation Diplômante.
+ */
+
+// Import de tout les utilitaires java.
+import java.util.*; 
+
+
+public class convertirTempereature { 
+    
+    
+    public static void main(String[] args) {
+        
+        // Permet de récupérer dans la console la saisie au clavier.
+        Scanner sc = new Scanner(System.in);
+        
+        // Permet d'afficher à la console le texte. 
+        System.out.println("Convertisseur des degrés Celcus et des degrés Fahrenheit");
+        System.out.println("--------------------------------------------------------");
+        
+        
+    }
+    
+    
+    
+}
